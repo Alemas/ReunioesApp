ca09ad2acf12110fde55e3ef6de158cefa243c68
NatashaFlores <natasha_ksf@outlook.com>
2015-05-07 10:11:15 -0300

    import Parse frameworks.

--- ReunioesApp/AppDelegate.swift
+++ ReunioesApp/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("adCK5M81sWruTioEdxDpg8raw9j487zeeQ2twRba",
+            clientKey: "AzAqAJqMkLj2SXjNw9wzszAcVf3qdg8jf3j1flVh")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
