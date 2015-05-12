cbe8dcb77b714b569050fe583bc729ecab004cf1
Uriel <urielbattanoli@hotmail.com>
2015-05-12 14:23:20 -0300

    present

--- ReunioesApp/AppDelegate.swift
+++ ReunioesApp/AppDelegate.swift
@@ -13,11 +13,11 @@ import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
-
+    
     var window: UIWindow?
     var locationManager = CLLocationManager()
     var beaconRegion = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "93069B63-F90A-4F7C-85F8-72132FFA9966"), identifier: "imeeting")
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -56,35 +56,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     }
     
     func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
-        for beacon in beacons{
-            let array = [(beacon as! CLBeacon).minor.integerValue, (beacon as! CLBeacon).major.integerValue]
-            let meetings = User.getMeetings()
-            for meet in meetings{
-                if((meet as! Meeting).minorAndMajor == array){
-                    //colocar no parse que estou presente
-                }
-            }
-        }
+//        if(User.getCurrentUser() != nil){
+//            for beacon in beacons{
+//                let meet = User.getMeetingForMinorAndMajor((beacon as! CLBeacon).minor.integerValue, (beacon as! CLBeacon).major.integerValue)
+//                var query = PFQuery(className: "Meeting_User")
+//                query.whereKey("user", equalTo: User.getCurrentUser()!)
+//                query.whereKey("meeting", equalTo: meet)
+//                let result:NSArray = query.findObjects()!
+//                if(result.count>0){
+//                    var obj:PFObject = result[0] as! PFObject
+//                    obj["present"]=true
+//                    obj.save()
+//                }
+//            }
+//        }
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
@@ -110,7 +115,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
             PFAnalytics.trackAppOpenedWithRemoteNotificationPayload(userInfo)
         }
     }
-
-
+    
+    
 }
 
--- ReunioesApp/iBeaconTransmitterViewController.swift
+++ ReunioesApp/iBeaconTransmitterViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreLocation
 import CoreBluetooth
+import Parse
 
 let uuid = NSUUID(UUIDString: "93069B63-F90A-4F7C-85F8-72132FFA9966")
 let identifier = "imeeting"
@@ -16,18 +17,32 @@ let identifier = "imeeting"
 class iBeaconTransmitterViewController: UIViewController, CBPeripheralManagerDelegate {
     
     var peripheralManager: CBPeripheralManager?
-    var beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 1, minor: 1, identifier: identifier)
+    var beaconRegion : CLBeaconRegion?
+    
+    var major: Int?
+    var minor: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: CLBeaconMajorValue(self.major!), minor: CLBeaconMinorValue(self.minor!), identifier: identifier)
         self.peripheralManager = CBPeripheralManager(delegate: self, queue: dispatch_get_main_queue())
+        var timer = NSTimer.scheduledTimerWithTimeInterval(15, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+    }
+    
+    func update(){
+        var query = PFQuery(className: "Meeting_User")
+        //let meeting = User.getMeetingForMinorAndMajor
+        //query.whereKey("meeting", equalTo: meeting)
+        let result:NSArray = query.findObjects()!
+        
     }
     
     //MARK: CBPeripheralDelegate
     func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager!) {
         switch (peripheral.state) {
         case .PoweredOn:
-            var peripheralData = beaconRegion.peripheralDataWithMeasuredPower(-59)
+            var peripheralData = beaconRegion!.peripheralDataWithMeasuredPower(-59)
             self.peripheralManager!.startAdvertising((peripheralData as NSDictionary as! [NSObject:AnyObject]))
         default:
             break
